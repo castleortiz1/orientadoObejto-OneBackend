c3345b2727a5fd46f21b07bd6301cb1c67e53120
jonadevk <mamalidiaw@gmail.com>
2024-09-27 03:34:04 -0600

    primer commit de aplicando la orientacion a objetos

---
+++ src/Pelicula.java
@@ -0,0 +1,25 @@
+public class Pelicula {
+
+    String nombre;
+    int fechaDeLanzamineto;
+    int duracionEnMinutos;
+    boolean incluidoEnElPlan;
+    double sumaDeLasEvaluciones;
+    int totalDeLasEvaluaciones;
+
+    void muestraFichaTecnica(){
+        System.out.println("\nEl nombre de la pelicula es: " + nombre);
+        System.out.println("Su fecha de lanzamiento fue el: " + fechaDeLanzamineto);
+        System.out.println(duracionEnMinutos + " Minutos de Duracion.");
+    }
+
+    void evalua(double nota){
+        sumaDeLasEvaluciones += nota;
+        totalDeLasEvaluaciones++;
+
+    }
+
+    double calculaMedia(){
+            return sumaDeLasEvaluciones / totalDeLasEvaluaciones;
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,30 @@
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.nombre = "Encanto";
+        miPelicula.fechaDeLanzamineto = 2021;
+        miPelicula.duracionEnMinutos = 120;
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(9.2);
+        miPelicula.evalua(10);
+
+        System.out.println(miPelicula.sumaDeLasEvaluciones);
+        System.out.println(miPelicula.totalDeLasEvaluaciones);
+        System.out.println(miPelicula.calculaMedia());
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "La La Land";
+        otraPelicula.fechaDeLanzamineto = 2016;
+        otraPelicula.duracionEnMinutos = 125;
+
+        otraPelicula.muestraFichaTecnica();
+        otraPelicula.evalua(8.2);
+        otraPelicula.evalua(10);
+
+        System.out.println(otraPelicula.sumaDeLasEvaluciones);
+        System.out.println(otraPelicula.totalDeLasEvaluaciones);
+        System.out.println(otraPelicula.calculaMedia());
+
+    }
+}
